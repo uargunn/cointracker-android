1a7fe2857f48b544e6132ccac9c3343c3cf4abc1
Ufuk Argun (BT DANIŞMAN) <extern.ufuk-argun@allianz.com.tr>
2023-02-18 17:20:36 +0300

    Splash screen api has been implemented

--- app/src/main/java/com/example/cointracker_android/feature/presentation/MainActivity.kt
+++ app/src/main/java/com/example/cointracker_android/feature/presentation/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.cointracker_android.feature.presentation
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.activity.viewModels
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
@@ -10,6 +11,7 @@ import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import androidx.navigation.NavType
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
@@ -28,8 +30,15 @@ import javax.inject.Inject
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
 
+    private val viewModel: MainViewModel by viewModels()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        installSplashScreen().apply {
+            setKeepOnScreenCondition {
+                viewModel.isLoading.value
+            }
+        }
         setContent {
             CointrackerandroidTheme {
                 // A surface container using the 'background' color from the theme
